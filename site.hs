dcf81fdb7170b544356202f18ae666192758ad28
Craige McWhirter <craige@mcwhirter.com.au>
2014-10-28 11:13:26 +1100

    Added RSS and Atom feeds

--- site.hs
+++ site.hs
@@ -5,6 +5,15 @@ import           Hakyll
 
 
 --------------------------------------------------------------------------------
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration = FeedConfiguration
+    { feedTitle       = "mcwhirter.com.au"
+    , feedDescription = "ruagaire de inbhe, ruagaire de eòlas, ruagair de neo-cheangaltas"
+    , feedAuthorName  = "Craige McWhirter"
+    , feedAuthorEmail = "craige@mcwhirter.com.au"
+    , feedRoot        = "https://mcwhirter.com.au/"
+    }
+
 main :: IO ()
 main = hakyll $ do
     match ("images/*" .||. "*icon*") $ do
@@ -25,8 +34,27 @@ main = hakyll $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= saveSnapshot "content"
             >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
+
+    create ["atom.xml"] $ do
+        -- | Create the atom feed.
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+                loadAllSnapshots "craige/*/*/*" "content"
+            renderAtom myFeedConfiguration feedCtx posts
+
+    create ["index.rss"] $ do
+        -- | Create the RSS feed.
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+                loadAllSnapshots "craige/*/*/*" "content"
+            renderRss myFeedConfiguration feedCtx posts
 
     create ["archive.html"] $ do
         route idRoute
